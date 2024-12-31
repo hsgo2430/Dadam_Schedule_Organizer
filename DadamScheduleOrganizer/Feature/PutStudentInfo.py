308c89b303a82295dfde61a50776f1c2528b5aa8
jusang3057 <jusang3057@gmail.com>
2024-12-31 12:06:55 +0900

    Update PutStudentInfo.py

--- DadamScheduleOrganizer/Feature/PutStudentInfo.py
+++ DadamScheduleOrganizer/Feature/PutStudentInfo.py
@@ -5,52 +5,76 @@ from Model.Student import Worker
 
 def putStudentNameMajor(excel_file_path, student):
     load_wb = load_workbook(excel_file_path, data_only=True)
+    work_space = load_wb.active
 
-    for index, timeTable in enumerate(student.timeTables):
-        work_space = load_wb.worksheets[2*index]
+    if student.worker == Worker.EXISTING:
+        last_row = findLastRowIndex(work_space, 4, 67)
+        work_space[f"{chr(last_row)}4"] = student.name
 
-        if student.worker == Worker.EXISTING:
-            last_row = findLastRowIndex(work_space, 4, 67)
-            work_space[f"{chr(last_row)}4"] = student.name
-
-        elif student.worker == Worker.NEW:
-            last_row = findLastRowIndex(work_space, 6, 67)
-            work_space[f"{chr(last_row)}6"] = student.name
+    elif student.worker == Worker.NEW:
+        last_row = findLastRowIndex(work_space, 6, 67)
+        work_space[f"{chr(last_row)}6"] = student.name
 
     load_wb.save(excel_file_path)
 
 
 def putStudnetTimeSchedule(excel_file_path, student):
     load_wb = load_workbook(excel_file_path, data_only=True)
-    for index, timeTable in enumerate(student.timeTables):
-        work_space = load_wb.worksheets[2*index]
+    work_space = load_wb.active
 
-        if student.worker == Worker.NEW:
-            first_row = 68
-            second_row = 69
-            # 아스키 코드 D, E
+    if student.worker == Worker.NEW:
+        first_row = 68
+        second_row = 69
+        # 아스키 코드 D, E
 
-        elif student.worker == Worker.EXISTING:
-            first_row = 66
-            second_row = 67
-            # 아스키 코드 B, C
+    elif student.worker == Worker.EXISTING:
+        first_row = 66
+        second_row = 67
+        # 아스키 코드 B, C
 
-        for week_time_table in timeTable:
-            # 월 화 수 목 금
-            time_col = 11  # B11, C11, F11, G11 부터 시작
-            for day_index, day_time_table in enumerate(week_time_table):
-                if day_time_table != "":
+    for week_time_table in student.timeTable:
+        # 월 화 수 목 금
+        time_col = 11  # B11, C11, F11, G11 부터 시작
+        for day_index, day_time_table in enumerate(week_time_table):
+            if day_time_table != "":
 
-                    last_first_col = findLastColIndex(work_space, time_col, first_row)
-                    last_second_col = findLastColIndex(work_space, time_col, second_row)
+                last_first_col = findLastColIndex(work_space, time_col, first_row)
+                last_second_col = findLastColIndex(work_space, time_col, second_row)
 
-                    if last_first_col == last_second_col:
-                        work_space[f"{chr(first_row)}{last_first_col}"] = student.name
-                    else:
-                        work_space[f"{chr(second_row)}{last_second_col}"] = student.name
-                time_col += 8
+                if last_first_col == last_second_col:
+                    work_space[f"{chr(first_row)}{last_first_col}"] = student.name
+                else:
+                    work_space[f"{chr(second_row)}{last_second_col}"] = student.name
+            time_col += 8
 
-            first_row += 4
-            second_row += 4
+        first_row += 4
+        second_row += 4
     load_wb.save(excel_file_path)
 
+# --근무시간표 정렬-- 2024-12-31 추가(김주상)
+def sortStudentTimeSchecule(excel_file_path):
+    load_wb = load_workbook(excel_file_path, data_only=True)
+    work_space = load_wb.active
+
+    # 해당 문자열이면 cell이 비였다고 판단
+    empty = [None, "", " "]
+
+    for row_start in range(11, 75, 8):
+        for col_start in range(2, 22, 2):
+            data = []
+            # 16칸에 대한 모든 근무자 정보를 data에 넣고 기존 값 삭제
+            for row in range(row_start, row_start+8):
+                for col in range(col_start, col_start+2):
+                    cell_value = work_space.cell(row=row, column=col).value
+                    if cell_value not in empty:
+                        data.append(cell_value)
+                    work_space.cell(row=row, column=col).value = None
+            # data에 있는 값을 위에서부터 채움
+            data_index = 0
+            for row in range(row_start, row_start+8):
+                for col in range(col_start, col_start+2):
+                    if data_index < len(data):
+                        work_space.cell(row=row, column=col).value = data[data_index]
+                        data_index += 1
+
+    load_wb.save(excel_file_path)
